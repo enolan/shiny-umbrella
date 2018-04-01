b02c4e1e3a33143970573c21ab7212a9101564f5
Echo Nolan <echo@echonolan.net>
2018-04-01 16:28:50 -0700

    YAPFy

--- client/client.py
+++ client/client.py
@@ -6,43 +6,50 @@ from natlinkutils import GrammarBase
 
 natlink.natConnect(True)
 
+
 def getAllWords(resObj):
-  choices = []
-  n = 0
-  while True:
-    try:
-      choices.append(resObj.getWords(n))
-    except natlink.OutOfRange:
-      return choices
-    n += 1
+    choices = []
+    n = 0
+    while True:
+        try:
+            choices.append(resObj.getWords(n))
+        except natlink.OutOfRange:
+            return choices
+        n += 1
+
 
 # Set up a catchall grammar. The whole idea is to forward everything to the
 # server, so we don't do any processing here.
 class CatchallGrammar(GrammarBase):
-  def __init__(self):
-    GrammarBase.__init__(self)
-    GrammarBase.load(self,
-	                 """<dgndictation> imported;
-                         <catchall> exported = <dgndictation>;""",
-					 allResults=1,
-           hypothesis=1)
+    def __init__(self):
+        GrammarBase.__init__(self)
+        GrammarBase.load(
+            self,
+            """<dgndictation> imported;
+               <catchall> exported = <dgndictation>;""",
+            allResults=1,
+            hypothesis=1)
 
-  def gotResultsObject(self, recogType, resObj):
-    print(type(resObj))
-    print("recogType {}, choices {}".format(recogType, getAllWords(resObj)))
+    def gotResultsObject(self, recogType, resObj):
+        print(type(resObj))
+        print(
+            "recogType {}, choices {}".format(recogType, getAllWords(resObj)))
 
-  def gotHypothesis(self, words):
-    print("hypothesis {}".format(words))
+    def gotHypothesis(self, words):
+        print("hypothesis {}".format(words))
+
 
 grammar = CatchallGrammar()
 grammar.activate("catchall", exclusive=1)
 
 loaded = True
 
+
 def printLoop():
-  while loaded:
-    print("loop")
-    time.sleep(1)
+    while loaded:
+        print("loop")
+        time.sleep(1)
+
 
 loopThread = Thread(target=printLoop)
 loopThread.start()
